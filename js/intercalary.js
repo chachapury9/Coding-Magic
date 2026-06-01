a5cc7c4ea84b737d38e86ad817be1fa8a7cc02a9
chachapury9 <kolyario@outlook.com>
2026-06-01 14:08:59 +0300

    style done for one section, help me

--- js/intercalary.js
+++ js/intercalary.js
@@ -1,13 +1,14 @@
-"use strict";
 const input = document.getElementById("iyc-input");
 const btn = document.getElementById("iyc-btn");
-const result = document.querySelector(".iyc-conclusion");
+const result = document.getElementById("iyc-conclusion");
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   if (input.value % 4 === 0) {
     result.textContent = "Ви народилися у високосний рік!";
+    result.style.color = "#039900";
   } else {
-    result.textContent = "Ви народилися не у високосний рік.";
+    result.textContent = "Ви народилися не у високосний рік!";
+    result.style.color = "#990000";
   }
 });
