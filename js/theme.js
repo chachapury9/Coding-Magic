5c5da8d33ede90f8bfdb3c01ce64acbd8c6dd496
chachapury9 <kolyario@outlook.com>
2026-06-14 10:54:34 +0300

    added a theme button and a footer.

--- js/theme.js
+++ js/theme.js
@@ -2,12 +2,14 @@ const themeBtn = document.getElementById("themeSwitch");
 let state = "white";
 const inputs = document.querySelectorAll("input");
 const btns = document.querySelectorAll("button");
+const themeVisual = document.getElementById("theme-visual");
 
 themeBtn.addEventListener("click", () => {
   switch (state) {
     case "white":
       state = "black";
       console.log(state);
+      themeVisual.setAttribute("href", "./images/Group72.svg");
       document.body.classList.add("black");
       document.body.querySelector("nav").classList.add("black");
       document.body.querySelector(".nav-list").classList.add("black");
@@ -24,11 +26,14 @@ themeBtn.addEventListener("click", () => {
           .classList.remove("white-border");
         document.getElementById("team-right").classList.remove("white-border");
         document.getElementById("team-left").classList.remove("white-border");
+
+        themeBtn.classList.remove("white-border");
       });
       break;
     case "black":
       state = "white";
       console.log(state);
+      themeVisual.setAttribute("href", "./images/Group71.svg");
       document.body.classList.remove("black");
       document.body.querySelector("header").classList.remove("black");
       document.body.querySelector("nav").classList.remove("black");
